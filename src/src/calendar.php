95dc5fc8b4c8bcebd9a9c69050bb5c13762c0244
--help <you@example.com>
2025-07-05 16:02:06 +0200

    Add event editing functionality with validation and database update in `calendar.php`

--- src/src/calendar.php
+++ src/src/calendar.php
@@ -36,6 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" &&
         exit;
     }
 
+    /** @var mysqli $connection */
     $stmt = $connection->prepare("INSERT INTO events (title, date, start_time, end_time) VALUES (?, ?, ?, ?)");
     $stmt->bind_param("ssss", $event, $date, $startTime, $endTime);
     $stmt->execute();
@@ -44,4 +45,44 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" &&
     header("Location: /" . $_SERVER["PHP_SELF"] . "/success=true");
     exit;
 
-}
+}//CREATE
+
+
+if ($_SERVER["REQUEST_METHOD"] === "POST" &&
+    isset($_POST["form-action"]) &&
+    $_POST["form-action"] === "edit"
+) {
+    $eventId = $_POST["event-edit-id"];
+    $event = trim($_POST["event-title"]);
+    $date = trim($_POST["event-date"]);
+    $startTime = trim($_POST["event-start-time"]);
+    $endTime = trim($_POST["event-end-time"]);
+
+    if (empty($eventId) ||empty($event) || empty($date) || empty($startTime) || empty($endTime)) {
+        $errorMessage = "All fields are required";
+        header("Location: /" . $_SERVER["PHP_SELF"] . "/error=true");
+        exit;;
+    }
+
+    if (!preg_match("/^\d{4}-\d{2}-\d{2}$/", $date)) {
+        $errorMessage = "Invalid date format";
+        header("Location: /" . $_SERVER["PHP_SELF"] . "/error=true");
+        exit;
+    }
+
+    if (!preg_match("/^([01][0-9]|2[0-3]):[0-5][0-9]$/", $startTime) ||
+        !preg_match("/^([01][0-9]|2[0-3]):[0-5][0-9]$/", $endTime)) {
+        $errorMessage = "Invalid time format";
+        header("Location: /" . $_SERVER["PHP_SELF"] . "/error=true");
+        exit;
+    }
+
+    /** @var mysqli $connection */
+    $stmt = $connection->prepare("UPDATE events SET title = ?, date = ?, start_time = ?, end_time = ? WHERE id = ?");
+    $stmt->bind_param("ssssi", $event, $date, $startTime, $endTime, $eventId);
+    $stmt->execute();
+    $stmt->close();
+
+    header("Location: /" . $_SERVER["PHP_SELF"] . "/success=true");
+    exit;
+}  //EDIT
