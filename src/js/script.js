a7fff26d1ddf98ad3960e7335560f79e7847a262
--help <you@example.com>
2025-07-05 17:10:43 +0200

    Add dynamic calendar rendering with event display, creation, and editing logic in `script.js`

---
+++ src/js/script.js
@@ -0,0 +1,127 @@
+//define
+const calendarElement = document.getElementById('calendar');
+const monthYearElement = document.getElementById('month-year');
+const modalElement = document.getElementById('modal');
+const currentDate = new Date();
+const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+function renderCalendar(date = new Date()) {
+    calendarElement.innerHTML = '';
+
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const today = currentDate
+
+    const totalDays = new Date(year, month + 1, 0).getDate();
+    const firstDay = new Date(year, month, 1).getDay();
+
+    monthYearElement.textContent = date.toLocaleString('default', {month: 'long', year: 'numeric'});
+    monthYearElement.textContent = date.toLocaleString('default', {month: 'long', year: 'numeric'});
+
+    weekDays.forEach(day => {
+        const dayElement = document.createElement('div');
+        dayElement.className = "day-name";
+        dayElement.textContent = day;
+        calendarElement.appendChild(dayElement);
+    })
+
+    for (let i = 0; i < firstDay; i++) {
+        calendarElement.appendChild(document.createElement('div'));
+    }
+
+    for (let day = 1; day <= totalDays; day++) {
+        const dayString = `${year}-${String(month + 1).padStart(2, '0')}-${String(day).padStart(2, "0")}`;
+
+        const dayCell = document.createElement('div')
+        dayCell.className = "day";
+
+        if (today.getDate() === day && today.getMonth() === month && today.getFullYear() === year) {
+            dayCell.classList.add('today');
+        }
+
+        const dateElement = document.createElement('div')
+        dateElement.className = "day-number";
+        dateElement.textContent = String(day);
+        dayCell.appendChild(dateElement);
+
+        const eventToday = events.filter(e => e.date === dayString)
+        const eventBox = document.createElement('div');
+        eventBox.className = "events"
+
+        eventToday.forEach(e => {
+            const event = document.createElement('div');
+            event.className = "event";
+
+            const eventTitle = document.createElement('div');
+            eventTitle.className = "event-title";
+            eventTitle.textContent = e.title;
+
+            const evenTime = document.createElement('div');
+            evenTime.className = "event-time";
+            evenTime.textContent = e.startTime + "-" + e.endTime;
+
+            event.appendChild(eventTitle);
+            event.appendChild(evenTime);
+
+            eventBox.appendChild(event);
+        })
+
+
+        //overlay
+        const overlay = document.createElement('div');
+        overlay.className = "day-overlay";
+
+        const addButton = document.createElement('button');
+        addButton.className = "btn-overlay";
+        addButton.textContent = "Add Event";
+
+        addButton.onclick(
+            (e) => {
+                e.stopPropagation();
+                openModalCreate(dayString)
+            }
+        )
+
+        overlay.appendChild(addButton);
+
+        if (eventToday.length > 0) {
+            const editButton = document.createElement('button');
+            editButton.className = "btn-overlay";
+            editButton.textContent = "Edit Event";
+
+            editButton.onclick(
+                (e) => {
+                    e.stopPropagation();
+                    openModalEdit(eventToday)
+                }
+            )
+            overlay.appendChild(eventToday);
+        }
+
+        dayCell.appendChild(overlay);
+        dayCell.appendChild(eventBox);
+        calendarElement.appendChild(dayCell);
+    }
+}
+
+function openModalCreate(date) {
+    document.getElementById("form-action").value = "add";
+    document.getElementById("event-create-id").value = "";
+    document.getElementById("event-edit-id").value = "";
+    document.getElementById("event-delete-id-id").value = "";
+    document.getElementById("event-title").value = "";
+    document.getElementById("event-date").value = date;
+    document.getElementById("event-start-time").value = "09:00";
+    document.getElementById("event-end-time").value = "10:00";
+
+    const selector = document.getElementById("eventSelector");
+    const selectorWrapper = document.getElementById("eventSelectorWrapper");
+
+    if (selector && selectorWrapper) {
+        selector.innerHTML = "";
+        selectorWrapper.style.display = "none";
+    }
+
+    modalElement.style.display = "flex";
+}
+
